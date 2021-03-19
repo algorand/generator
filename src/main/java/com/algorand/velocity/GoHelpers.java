505087a36a1572344c6f91ef5528522814107ded
Will Winder <wwinder.unh@gmail.com>
2021-03-19 05:26:26 -0400

    Move second macro to Java.

--- src/main/java/com/algorand/velocity/GoHelpers.java
+++ src/main/java/com/algorand/velocity/GoHelpers.java
@@ -4,9 +4,9 @@ public class GoHelpers {
     /**
      * Map the generated query type to the hand written type.
      * @param input generated query type.
-     * @return backwards compatible hand written type.
+     * @return backwards compatible with written type.
      */
-    public String queryTypeMapper(String  input) {
+    public String queryTypeMapper(String input) {
         switch(input) {
             case "SearchForAccounts":               return "SearchAccounts";
             case "GetStatus":                       return "Status";
@@ -16,4 +16,18 @@ public class GoHelpers {
         }
     }
 
+    /**
+     * Map the generated return type to the hand written type.
+     * @param input generated return type.
+     * @return backwards compatible with hand written type.
+     */
+    public String returnTypeMapper(String input) {
+        switch(input) {
+            case "NodeStatusResponse":          return "NodeStatus";
+            case "PendingTransactionResponse":  return "PendingTransactionInfo";
+            default: return input;
+        }
+    }
+
+    //public string
 }
