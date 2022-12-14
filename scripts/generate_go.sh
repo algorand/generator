2400f906c8343ab9575dfd9850a0cbb335313dc9
algochoi <86622919+algochoi@users.noreply.github.com>
2022-12-14 12:05:17 -0500

    Revert path

--- scripts/generate_go.sh
+++ scripts/generate_go.sh
@@ -14,10 +14,10 @@ function help() {
   exit
 }
 
-ALGOD_SPEC=~/go/src/github.com/algorand/go-algorand/daemon/algod/api/algod.oas2.json
-INDEXER_SPEC=~/go/src/github.com/algorand/indexer/api/indexer.oas2.json
-SDK_DIR=/Users/andrew/Desktop/projects/go-algorand-sdk
-TEMPLATE_DIR=/Users/andrew/go/src/github.com/algorand/generator/go_templates
+ALGOD_SPEC=/home/will/go/src/github.com/algorand/go-algorand/daemon/algod/api/algod.oas2.json
+INDEXER_SPEC=/home/will/algorand/indexer/api/indexer.oas2.json
+SDK_DIR=/home/will/go/src/github.com/algorand/go-algorand-sdk
+TEMPLATE_DIR=/home/will/algorand/generator/go_templates
 
 PARAMS=""
 
